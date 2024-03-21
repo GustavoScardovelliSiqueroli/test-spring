a69ee1e278734a4d1116e7195dcb6bf106f84c14
Gustavo Scardovelli Siqueroli <gustavosecardovelli@gmail.com>
2024-03-21 17:16:20 -0300

    feature: new roules

--- src/main/java/com/testapi/first/controllers/ShirtController.java
+++ src/main/java/com/testapi/first/controllers/ShirtController.java
@@ -1,6 +1,9 @@
 package com.testapi.first.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.testapi.first.models.Shirt;
 import com.testapi.first.repositories.ShirtRepository;
+
 import com.testapi.first.dtos.ShirtRecordDto;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("shirts")
@@ -25,16 +32,38 @@ public class ShirtController {
     ShirtRepository shirtRepository;
 
     @GetMapping("/")
-    public ResponseEntity<List<Shirt>> getMethodName() {
+    public ResponseEntity<List<Shirt>> getShirts() {
         return ResponseEntity.status(HttpStatus.OK).body(shirtRepository.findAll());
     }
 
     @PostMapping("/")
-    public ResponseEntity<Shirt> postMethodName(@RequestBody @Valid ShirtRecordDto shirtRecordDto) {
+    public ResponseEntity<Shirt> createShirt(@RequestBody @Valid ShirtRecordDto shirtRecordDto) {
         var shirt = new Shirt();
         BeanUtils.copyProperties(shirtRecordDto, shirt);
         return ResponseEntity.status(HttpStatus.CREATED).body(shirtRepository.save(shirt));
 
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteShirt(@PathVariable(value = "id") UUID id) {
+        Optional<Shirt> shirt = shirtRepository.findById(id);
+        if (shirt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
+        }
+        shirtRepository.delete(shirt.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateShirt(@PathVariable UUID id,
+            @RequestBody @Valid ShirtRecordDto shirtRecordDto) {
+        Optional<Shirt> shirt = shirtRepository.findById(id);
+        if (shirt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produc not found.");
+        }
+        var shirtModel = shirt.get();
+        BeanUtils.copyProperties(shirtRecordDto, shirt);
+        return ResponseEntity.status(HttpStatus.OK).body(shirtRepository.save(shirtModel));
     }
 
 }
--- src/main/java/com/testapi/first/controllers/TestController.java
+++ src/main/java/com/testapi/first/controllers/TestController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 import java.util.HashMap;
 
+
 @RestController
 public class TestController {
-    @GetMapping("/")
+    @GetMapping("/") 
     public ResponseEntity<Map<String, String>> getTest() {
         Map<String, String> test = new HashMap<>();
         test.put("teste", "oiii");
-        test.put("cuzinho", "oiii");
+        test.put("peidei", "molhado");
 
         return ResponseEntity.status(HttpStatus.OK).body(test);
     }
